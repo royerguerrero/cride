cf6c05e6b17bc78716461348087caf7238b2fa4e
machine 3 <gear.dev1@fuziontek.co>
2020-08-02 21:36:20 -0500

    Add serializers

---
+++ cride/circles/serializers.py
@@ -0,0 +1,8 @@
+"""Circle serializers"""
+
+# Django REST Framerwork
+from rest_framework import serializers
+
+class CircleSerializer(serializers.Serializer):
+    """Circle serializer"""
+    pass
